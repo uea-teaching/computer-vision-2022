326f9b6664c3158e622a90a5a8703826ba1992ef
Dave Greenwood <dgrnwd@gmail.com>
2022-02-27 12:55:39 +0000

    hog-example

---
+++ scripts/hog_example.py
@@ -0,0 +1,42 @@
+# %%
+import matplotlib.pyplot as plt
+
+from skimage.feature import hog
+from skimage import data, exposure
+
+
+def set_style():
+    """set the style of the plots"""
+    plt.style.use('fivethirtyeight')
+    plt.rcParams['figure.figsize'] = [9.0, 5.0]
+    plt.rcParams['figure.dpi'] = 240
+    plt.rcParams['axes.linewidth'] = 1.25
+    plt.rcParams['savefig.pad_inches'] = 0.2
+    plt.rcParams['savefig.bbox'] = 'tight'
+
+
+set_style()
+
+# %%
+
+image = data.astronaut()
+
+fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualize=True, multichannel=True)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
+
+ax1.axis('off')
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.set_title('Input image')
+
+# Rescale histogram for better display
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+ax2.axis('off')
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+ax2.set_title('Histogram of Oriented Gradients')
+
+fig.savefig("hog_example.png")
+
+# %%
